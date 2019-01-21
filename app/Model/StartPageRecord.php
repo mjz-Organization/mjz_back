92d9c0a7b86a899e3fe906db836911cd30b27cf8
edwin <1340639717@qq.com>
2019-01-21 22:28:33 +0800

    启动页管理

--- app/Model/StartPageRecord.php
+++ app/Model/StartPageRecord.php
@@ -3,7 +3,16 @@
 namespace App\Model;
 
 
+use Illuminate\Support\Facades\DB;
+
 class StartPageRecord extends BaseModel
 {
     protected $table = 'start_page_record';
+    private static $dbTable = 'start_page_record';
+
+    public static function getStartPageList($per_page = 5){
+        $pageList = DB::table(self::$dbTable)->paginate($per_page);
+        return $pageList;
+    }
+
 }
